aa0d5301d10c9fbb3ed1abad0777992be76c75e5
unknown <a.abdelazizg@gmail.com>
2022-06-26 06:36:08 +0200

    edit p tag in home

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,37 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Styles from './Home.module.css';
 
-const Home = () => {
-  
-  useEffect(()=>{
-    let text = 'F';
-    let myTitle = 'ront-End Developer';
-    let counter = 0;
-    function write(){
-      if (counter <= myTitle.length) {
-        counter++
-        text += Array.from(myTitle)[counter];
-      }
-
-      if(counter === myTitle.length ) {
-        counter = 0;
-        text = 'F';
-      }
-      
-      setTimeout(write, 400); 
-      document.getElementById('title').innerHTML = text;
-    };
-
-    write()
-  })
-
+const Home = () => {  
   return (
     <main className={Styles.main}>
       <section className={Styles.Home}>
           <div className={Styles.ovarlay}></div>
           <div className={Styles.caption}>
               <h1>Ahmed <span>Abdel-Aziz</span></h1>
-              <p id='title'></p>
+              <p>Front-End Developer</p>
           </div>
       </section>
     </main>
